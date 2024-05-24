ee0de35cd0b124a03d4fcd75ccf5d7d2e1d74d61
quan1410 <vuongquan14102004@gmail.com>
2024-05-25 00:31:17 +0700

    Fix all changes

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('account/register',views.register,name='register'),
     path('account/login',views.login,name='login'),
-     path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard',index,name='dashboard'),
     path('logout/', views.logout_user, name='logout'),
 ]
